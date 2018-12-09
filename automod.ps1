dd2bc09d28edc3ae8dc1d1cb639c3919380a6e10
Ben Nottelling <daprogramerforbatch@gmail.com>
2018-12-09 12:55:19 -0800

    CUI Tweaks, disable two other tweaks
    Hide the optimal resolution notification when you play non-native resolutions, and hide promotions in the graphics panel. Also, disable two tweaks because I have't really tested them

--- automod.ps1
+++ automod.ps1
@@ -1,7 +1,7 @@
 $host.ui.RawUI.WindowTitle = "AutoMod for Intel iGPU"
 #Check if we can find the inf file (I.E, are we in a driver folder?)
-if(Test-Path '.\Graphics\iigd_dch.inf' -PathType Leaf) { $inffile = '.\Graphics\iigd_dch.inf'}
-if(Test-Path '.\Graphics\igdlh64.inf' -PathType Leaf) { $inffile = '.\Graphics\igdlh64.inf'}
+if (Test-Path '.\Graphics\iigd_dch.inf' -PathType Leaf) { $inffile = '.\Graphics\iigd_dch.inf'}
+if (Test-Path '.\Graphics\igdlh64.inf' -PathType Leaf) { $inffile = '.\Graphics\igdlh64.inf'}
 
 #If we can't find the inf file then promt for it
 if (!$inffile) {$inffile = Read-Host -Prompt 'Please drag your driver INF file here and press enter'}
@@ -28,22 +28,47 @@ echo "`n `nChanged to maximum power configuration"
 }
 
 #Mark this file as modded by iGPU AutoMod
-(gc $inffile).replace('INF for the Intel Corporation graphics adapter.', 'INF for the Intel Corporation graphics adapter, modded by iGPU AutoMod') | Set-Content $inffile
+(gc $inffile).replace('INF for the Intel Corporation graphics adapter.', 'INF for the Intel Corporation graphics adapter, modified by iGPU AutoMod') | Set-Content $inffile
 
 #Change max mem config
 (gc $inffile) -replace 'MaximumDeviceMemoryConfiguration = 512', 'MaximumDeviceMemoryConfiguration = 1024' | Out-File $inffile
-echo "`nMaximumDeviceMemoryConfiguration = 512 --> 2048"
+echo "`nMaximumDeviceMemoryConfiguration = 512 --> 1024"
 
 #Optimizations Found in Drivers
-(gc $inffile) -replace 'HKR,, ForcePLLDisableEnableForFreqChange, %REG_DWORD%, 0x0', 'HKR,, ForcePLLDisableEnableForFreqChange, %REG_DWORD%, 0x1' | Out-File $inffile
+#(gc $inffile) -replace 'HKR,, ForcePLLDisableEnableForFreqChange, %REG_DWORD%, 0x0', 'HKR,, ForcePLLDisableEnableForFreqChange, %REG_DWORD%, 0x1' | Out-File $inffile #TBH I'm not sure what this one does
+#(gc $inffile) -replace 'HKR,, NoFastLinkTrainingForeDP, %REG_DWORD%, 1', 'HKR,, NoFastLinkTrainingForeDP, %REG_DWORD%, 0' | Out-File $inffile #I don't have DisplayPort on any of my deviced with an Intel iGPU to test this, if someone does, please do.
 echo "`nEnabled optimizations found in drivers"
 
 #Stolen from PHDGD drivers
-(gc $inffile) -replace 'HKR,, IncreaseFixedSegment,%REG_DWORD%, 0', 'HKR,, IncreaseFixedSegment,%REG_DWORD%, 1' | Out-File $inffile
-(gc $inffile) -replace 'HKR,, TotalStaticModes, %REG_DWORD%, 2', 'HKR,, TotalStaticModes, %REG_DWORD%,0' | Out-File $inffile
+#(gc $inffile) -replace 'HKR,, IncreaseFixedSegment,%REG_DWORD%, 0', 'HKR,, IncreaseFixedSegment,%REG_DWORD%, 1' | Out-File $inffile
+#(gc $inffile) -replace 'HKR,, TotalStaticModes, %REG_DWORD%, 2', 'HKR,, TotalStaticModes, %REG_DWORD%,0' | Out-File $inffile
 (gc $inffile) -replace 'HKR,, AdaptiveVsyncEnable,%REG_DWORD%, 1', 'HKR,, AdaptiveVsyncEnable,%REG_DWORD%, 0' | Out-File $inffile
 
 echo "`nMade changes from PHDGD drivers"
 
 echo "`nCompleted all mods, exit this window and install the new drivers"
+
+
+#Check if we can find the inf file for the CUI (Common User Interface)
+if (Test-Path '.\Graphics\cui_dch.inf' -PathType Leaf) {
+
+#Ask the user if they are okay with user-interface mods
+$message  = 'Enable CUI Mods?'
+$question = 'There are some CUI (Common User-Interface) mods we can perfom, would you like to use them?'
+
+$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+if ($decision -eq 0) {
+  #Modify the CUI
+  $string = ('HKR,,"ShowOptimalBalloon",%REG_DWORD%,0' + "`n" + 'HKR,,"ShowPromotions",%REG_DWORD%,0')
+  (gc '.\Graphics\cui_dch.inf') -replace 'HKR,,"ShowOptimalBalloon",%REG_DWORD%,1', $string | Out-File '.\Graphics\cui_dch.inf'
+  echo "`n `nModified the CUI"
+} else {
+  echo "`n `nSkipped CUI mods"
+}
+}
+echo "`nCompleted all modification. Feel free to close this and install the drivers"
 pause
--- readme.md
+++ readme.md
@@ -1,4 +1,14 @@
 # iGPU AutoMod
 A small tool to mod iGPU graphics drivers for maximum performance. GitHub repository is a mess that will be cleaned up, instructions are on the website 
 
-http://igpumods.weebly.com/
+## Supported Hardware
+ - Currently only Intel iGPUs are supported (CPU Generation 6-9, EG i5 6600K - i5 9600k, not limited to i5 only)
+
+## What Does it Change?
+ - Forces maximum power configuration (Laptop users should see a difference here)
+ - Changes the maximum VRAM limit to 1GB (from 512MB). Please note, this won't change anything but allow some games with VRAM limits to open
+ - Disable adaptive V-SYNC to "improve performance on games" (found in IntelliModder32's [TENSOR™ Driver](https://imo32.weebly.com))
+ - Hides the optimal resolution notification when you play non-native resolutions (I find this annoying)
+ - Hides promotions in the graphics panel (Why do we need ads on something we paid for)
+
+Official website: http://igpumods.weebly.com/
